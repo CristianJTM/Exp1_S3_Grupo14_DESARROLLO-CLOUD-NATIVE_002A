0fb7bc22f1f0278789f384e26a77fa30b1453316
CRISTIAN JAVIER TORRES MARTINEZ <ctorresm06@gmail.com>
2026-06-08 05:27:16 -0400

    Add DTO classes for transport management including ArchivoDTO, GuiaCreateDTO, GuiaDTO, GuiaResumenDTO, GuiaUpdateDTO, TransportistaDTO, and TransportistaResumenDTO

---
+++ src/main/java/com/duoc/transportmanagement/dto/GuiaResumenDTO.java
@@ -0,0 +1,66 @@
+package com.duoc.transportmanagement.dto;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class GuiaResumenDTO {
+    private Long id;
+
+    private Integer numeroGuia;
+
+    private LocalDate fechaGeneracion;
+
+    private String cliente;
+
+    private String transportista;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getNumeroGuia() {
+        return numeroGuia;
+    }
+
+    public void setNumeroGuia(Integer numeroGuia) {
+        this.numeroGuia = numeroGuia;
+    }
+
+    public LocalDate getFechaGeneracion() {
+        return fechaGeneracion;
+    }
+
+    public void setFechaGeneracion(LocalDate fechaGeneracion) {
+        this.fechaGeneracion = fechaGeneracion;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getTransportista() {
+        return transportista;
+    }
+
+    public void setTransportista(String transportista) {
+        this.transportista = transportista;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    private boolean estado;
+}
